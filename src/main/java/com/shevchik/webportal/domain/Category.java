3f9326bf3ece96106d6ece9fd00b793ad1d2b08b
shevchik <berrouz@gmail.com>
2012-10-06 15:05:42 +0200

    added domain models

--- src/main/java/com/shevchik/webportal/domain/Category.java
+++ src/main/java/com/shevchik/webportal/domain/Category.java
@@ -2,17 +2,18 @@ package com.shevchik.webportal.domain;
 
 import org.hibernate.annotations.ManyToAny;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "categories")
 public class Category {
+    @Id
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "name")
     private String name;
 
     @OneToMany
@@ -23,6 +24,7 @@ public class Category {
     private Set<Category> subCategories = new HashSet<>();
 
     @ManyToOne
+    @Column(name = "parent")
     private Category parentCategory;
 
 }
---
+++ src/main/java/com/shevchik/webportal/domain/OrderItem.java
@@ -0,0 +1,11 @@
+package com.shevchik.webportal.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shevchik
+ * Date: 06.10.12
+ * Time: 14:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrderItem {
+}
